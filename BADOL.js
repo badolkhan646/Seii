be7b007e24ed0f61ff25caa26faa74871c7d6e7a
badolkhan646 <141847046+badolkhan646@users.noreply.github.com>
2024-12-11 21:29:40 +0600

    Update BADOL.js

--- BADOL.js
+++ BADOL.js
@@ -253,7 +253,7 @@ if (config.autoRestart) {
 	global.utils.transporter = transporter;
 
 	// ———————————————— CHECK VERSION ———————————————— //
-	const { data: { version } } = await axios.get("https://raw.githubusercontent.com/ARYAN-BOT-404/Bestbot/refs/heads/main/package.json");
+	const { data: { version } } = await axios.get("https://raw.githubusercontent.com/mohammadbadol007/mohammadbadol007/refs/heads/main/package.json");
 	const currentVersion = require("./package.json").version;
 	if (compareVersion(version, currentVersion) === 1)
 		utils.log.master("NEW VERSION", getText(
