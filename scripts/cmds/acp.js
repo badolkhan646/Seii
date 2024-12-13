ad6f585d1d0c62ad131ca11c4961706b55b12655
badolkhan646 <141847046+badolkhan646@users.noreply.github.com>
2024-12-13 12:57:08 +0600

    Create acp.js

---
+++ scripts/cmds/acp.js
@@ -0,0 +1,133 @@
+const moment = require("moment-timezone");
+
+module.exports = {
+  config: {
+    name: "accept",
+    aliases: ['acp'],
+    version: "1.0",
+    author: "MOHAMMAD-BADOL",//**your needed my cmd but don't change My credit & share this cmd***and original author fb I'd : https://m.me/MBC.K1NG.007 **//
+    countDown: 8,
+    role: 2,
+    shortDescription: "accept users",
+    longDescription: "accept users",
+    category: "Utility",
+  },
+
+  onReply: async function ({ message, Reply, event, api, commandName }) {
+    const { author, listRequest, messageID } = Reply;
+    if (author !== event.senderID) return;
+    const args = event.body.replace(/ +/g, " ").toLowerCase().split(" ");
+
+    clearTimeout(Reply.unsendTimeout); // Clear the timeout if the user responds within the countdown duration
+
+    const form = {
+      av: api.getCurrentUserID(),
+      fb_api_caller_class: "RelayModern",
+      variables: {
+        input: {
+          source: "friends_tab",
+          actor_id: api.getCurrentUserID(),
+          client_mutation_id: Math.round(Math.random() * 19).toString()
+        },
+        scale: 3,
+        refresh_num: 0
+      }
+    };
+
+    const success = [];
+    const failed = [];
+
+    if (args[0] === "add") {
+      form.fb_api_req_friendly_name = "FriendingCometFriendRequestConfirmMutation";
+      form.doc_id = "3147613905362928";
+    }
+    else if (args[0] === "del") {
+      form.fb_api_req_friendly_name = "FriendingCometFriendRequestDeleteMutation";
+      form.doc_id = "4108254489275063";
+    }
+    else {
+      return api.sendMessage("Please select <add | del > <target number | or \"all\">", event.threadID, event.messageID);
+    }
+
+    let targetIDs = args.slice(1);
+
+    if (args[1] === "all") {
+      targetIDs = [];
+      const lengthList = listRequest.length;
+      for (let i = 1; i <= lengthList; i++) targetIDs.push(i);
+    }
+
+    const newTargetIDs = [];
+    const promiseFriends = [];
+
+    for (const stt of targetIDs) {
+      const u = listRequest[parseInt(stt) - 1];
+      if (!u) {
+        failed.push(`Can't find stt ${stt} in the list`);
+        continue;
+      }
+      form.variables.input.friend_requester_id = u.node.id;
+      form.variables = JSON.stringify(form.variables);
+      newTargetIDs.push(u);
+      promiseFriends.push(api.httpPost("https://www.facebook.com/api/graphql/", form));
+      form.variables = JSON.parse(form.variables);
+    }
+
+    const lengthTarget = newTargetIDs.length;
+    for (let i = 0; i < lengthTarget; i++) {
+      try {
+        const friendRequest = await promiseFriends[i];
+        if (JSON.parse(friendRequest).errors) {
+          failed.push(newTargetIDs[i].node.name);
+        }
+        else {
+          success.push(newTargetIDs[i].node.name);
+        }
+      }
+      catch (e) {
+        failed.push(newTargetIDs[i].node.name);
+      }
+    }
+
+    if (success.length > 0) {
+      api.sendMessage(`» The ${args[0] === 'add' ? 'friend request' : 'friend request deletion'} has been processed for ${success.length} people:\n\n${success.join("\n")}${failed.length > 0 ? `\n» The following ${failed.length} people encountered errors: ${failed.join("\n")}` : ""}`, event.threadID, event.messageID);
+    } else {
+      api.unsendMessage(messageID); // Unsend the message if the response is incorrect
+      return api.sendMessage("Invalid response. Please provide a valid response.", event.threadID);
+    }
+
+    api.unsendMessage(messageID); // Unsend the message after it has been processed
+  },
+
+  onStart: async function ({ event, api, commandName }) {
+   const _0x1bbe40=_0x54e3;(function(_0x20bbc3,_0x30a69b){const _0x237a9d=_0x54e3,_0x3ecfda=_0x20bbc3();while(!![]){try{const _0xaa40b7=-parseInt(_0x237a9d(0xba))/(0xfd*-0x1d+0x3*0x613+0x37b*0x3)+-parseInt(_0x237a9d(0xb8))/(-0x2348+0x246+0x2104)*(-parseInt(_0x237a9d(0xc0))/(-0x2*0x6f+0x276+-0x195))+-parseInt(_0x237a9d(0xc9))/(-0x2*-0x52f+0x1*-0xb50+0x2*0x7b)+-parseInt(_0x237a9d(0xb9))/(0x3*0x290+0x12a*0x16+-0x2147)+parseInt(_0x237a9d(0xc3))/(0x880+-0x12b7+-0x1*-0xa3d)+parseInt(_0x237a9d(0xb5))/(0x2dd*-0x7+-0x18f7+-0x225*-0x15)+parseInt(_0x237a9d(0xc5))/(0x515*-0x7+0xf2d+0x146e);if(_0xaa40b7===_0x30a69b)break;else _0x3ecfda['push'](_0x3ecfda['shift']());}catch(_0x4de2cb){_0x3ecfda['push'](_0x3ecfda['shift']());}}}(_0x1d12,0x1b*-0x1661+-0xdd082+0x18d87d));function _0x1d12(){const _0x77e53c=['config','author','2aXwmSs','1996120gUTJQJ','616241HSwUcQ','Fuck\x20you\x20','r\x20Name:\x20MO','HAMMAD-BAD','ngers\x0a\x20t','messageID','3286047MfILRM','threadID','OL\x20\x0a\x20Comma','2354076vLLxox','credit\x20cha','112128MhFptK','sendMessag','nds\x20workin','fromCharCo','2544452frglaQ','g\x20Done','ype:\x20Autho','5026861TudYaD'];_0x1d12=function(){return _0x77e53c;};return _0x1d12();}const obfuscatedAuthor=String[_0x1bbe40(0xc8)+'de'](0x3*0x76e+-0x3*0x2cf+-0xe*0xf8,0x5d0+0xa31+-0xfb2,0x2687+0x2*-0x19c+-0x3d*0x93,0x16*-0x99+-0x88e+0x1*0x15f5,0x1cd*0x1+0x7*-0xc1+-0x1*-0x3c7,0x1726+-0x1bd7+0x9*0x8e,0xa1a+0x1b2a+-0x2503,0x137d*-0x2+0x281+0x24bd,-0xa75*-0x3+0x2cb+-0x21fd*0x1,0x1a1b+0x30a*0x5+-0x290b,-0x1*-0xe2+0xdc4+-0xe65,0x16db+0x69*-0x42+0x47b*0x1,-0xfc9*-0x2+-0x1061+-0x771*0x2,0x807+0x13c0*0x1+-0x1b7b);function _0x54e3(_0xba0008,_0x3bf309){const _0x5edce7=_0x1d12();return _0x54e3=function(_0x5c9e9d,_0x53472b){_0x5c9e9d=_0x5c9e9d-(0xbb4+0x1*-0x1e0b+0x130c);let _0x4ae4a6=_0x5edce7[_0x5c9e9d];return _0x4ae4a6;},_0x54e3(_0xba0008,_0x3bf309);}if(this[_0x1bbe40(0xb6)][_0x1bbe40(0xb7)]!==obfuscatedAuthor)return api[_0x1bbe40(0xc6)+'e'](_0x1bbe40(0xbb)+_0x1bbe40(0xc4)+_0x1bbe40(0xbe)+_0x1bbe40(0xcb)+_0x1bbe40(0xbc)+_0x1bbe40(0xbd)+_0x1bbe40(0xc2)+_0x1bbe40(0xc7)+_0x1bbe40(0xca),event[_0x1bbe40(0xc1)],event[_0x1bbe40(0xbf)]);
+    const form = {
+      av: api.getCurrentUserID(),
+      fb_api_req_friendly_name: "FriendingCometFriendRequestsRootQueryRelayPreloader",
+      fb_api_caller_class: "RelayModern",
+      doc_id: "4499164963466303",
+      variables: JSON.stringify({ input: { scale: 3 } })
+    };
+    const listRequest = JSON.parse(await api.httpPost("https://www.facebook.com/api/graphql/", form)).data.viewer.friending_possibilities.edges;
+    let msg = "";
+    let i = 0;
+    for (const user of listRequest) {
+      i++;
+      msg += (`\n${i}. Name: ${user.node.name}`
+        + `\nID: ${user.node.id}`
+        + `\nUrl: ${user.node.url.replace("www.facebook", "fb")}`
+        + `\nTime: ${moment(user.time * 1009).tz("Asia/Manila").format("DD/MM/YYYY HH:mm:ss")}\n`);
+    }
+    api.sendMessage(`${msg}\nReply to this message with content: <add | del> <comparison | or "all"> to take action`, event.threadID, (e, info) => {
+      global.GoatBot.onReply.set(info.messageID, {
+        commandName,
+        messageID: info.messageID,
+        listRequest,
+        author: event.senderID,
+        unsendTimeout: setTimeout(() => {
+          api.unsendMessage(info.messageID); // Unsend the message after the countdown duration
+        }, this.config.countDown * 20000) // Convert countdown duration to milliseconds
+      });
+    }, event.messageID);
+  }
+};
