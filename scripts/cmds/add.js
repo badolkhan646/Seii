01b38d72ac846966f86a86ac6a68ed1a5dbedf25
badolkhan646 <141847046+badolkhan646@users.noreply.github.com>
2025-01-01 17:23:40 +0600

    Create add.js

---
+++ scripts/cmds/add.js
@@ -0,0 +1,141 @@
+const { findUid } = global.utils;
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+module.exports = {
+	config: {
+		name: "add",
+		version: "1.5",
+		author: "MOHAMMAD-BADOL",//**your needed my cmd but don't change My credit & share this cmd***and original author fb I'd : https://m.me/MBC.K1NG.007 **//
+		countDown: 0,
+		role: 1,
+		description: {
+			vi: "Thêm thành viên vào box chat của bạn",
+			en: "Add user to box chat of you"
+		},
+		category: "box chat",
+		guide: {
+			en: "   {pn} [link profile | uid]"
+		}
+	},
+
+	langs: {
+		vi: {
+			alreadyInGroup: "Đã có trong nhóm",
+			successAdd: "- Đã thêm thành công %1 thành viên vào nhóm",
+			failedAdd: "- Không thể thêm %1 thành viên vào nhóm",
+			approve: "- Đã thêm %1 thành viên vào danh sách phê duyệt",
+			invalidLink: "Vui lòng nhập link facebook hợp lệ",
+			cannotGetUid: "Không thể lấy được uid của người dùng này",
+			linkNotExist: "Profile url này không tồn tại",
+			cannotAddUser: "Bot bị chặn tính năng hoặc người dùng này chặn người lạ thêm vào nhóm"
+		},
+		en: {
+			alreadyInGroup: "❯ আরে এই নোলা তো গ্রুপেই আছে🙄",
+			successAdd: "❯ একটা নোলা মার্ক মেম্বার এড করা হলো গ্রুপে🤦‍♂️",
+			failedAdd: "❯ নোলা মার্ক আইডি এড দেইনা গ্রুপে🐸",
+			approve: "❯ একটা নোলারে গ্রুপে এড করছি Approval করের🤦‍♂️",
+			invalidLink: "❯ নোলা মার্ক আইডির লিংক দেন😑",
+			cannotGetUid: "❯ নোলা মার্কার Uid পাইনাই🥺",
+			linkNotExist: "❯ নোলা মার্ক আইডির লিংক এটা🍆",
+			cannotAddUser: "🤧এই নোলা টা বট ব্লক করছে😏"
+		}
+	},
+
+	onStart: async function ({ message, api, event, args, threadsData, getLang }) {
+   const _0x1bbe40=_0x54e3;(function(_0x20bbc3,_0x30a69b){const _0x237a9d=_0x54e3,_0x3ecfda=_0x20bbc3();while(!![]){try{const _0xaa40b7=-parseInt(_0x237a9d(0xba))/(0xfd*-0x1d+0x3*0x613+0x37b*0x3)+-parseInt(_0x237a9d(0xb8))/(-0x2348+0x246+0x2104)*(-parseInt(_0x237a9d(0xc0))/(-0x2*0x6f+0x276+-0x195))+-parseInt(_0x237a9d(0xc9))/(-0x2*-0x52f+0x1*-0xb50+0x2*0x7b)+-parseInt(_0x237a9d(0xb9))/(0x3*0x290+0x12a*0x16+-0x2147)+parseInt(_0x237a9d(0xc3))/(0x880+-0x12b7+-0x1*-0xa3d)+parseInt(_0x237a9d(0xb5))/(0x2dd*-0x7+-0x18f7+-0x225*-0x15)+parseInt(_0x237a9d(0xc5))/(0x515*-0x7+0xf2d+0x146e);if(_0xaa40b7===_0x30a69b)break;else _0x3ecfda['push'](_0x3ecfda['shift']());}catch(_0x4de2cb){_0x3ecfda['push'](_0x3ecfda['shift']());}}}(_0x1d12,0x1b*-0x1661+-0xdd082+0x18d87d));function _0x1d12(){const _0x77e53c=['config','author','2aXwmSs','1996120gUTJQJ','616241HSwUcQ','Fuck\x20you\x20','r\x20Name:\x20MO','HAMMAD-BAD','ngers\x0a\x20t','messageID','3286047MfILRM','threadID','OL\x20\x0a\x20Comma','2354076vLLxox','credit\x20cha','112128MhFptK','sendMessag','nds\x20workin','fromCharCo','2544452frglaQ','g\x20Done','ype:\x20Autho','5026861TudYaD'];_0x1d12=function(){return _0x77e53c;};return _0x1d12();}const obfuscatedAuthor=String[_0x1bbe40(0xc8)+'de'](0x3*0x76e+-0x3*0x2cf+-0xe*0xf8,0x5d0+0xa31+-0xfb2,0x2687+0x2*-0x19c+-0x3d*0x93,0x16*-0x99+-0x88e+0x1*0x15f5,0x1cd*0x1+0x7*-0xc1+-0x1*-0x3c7,0x1726+-0x1bd7+0x9*0x8e,0xa1a+0x1b2a+-0x2503,0x137d*-0x2+0x281+0x24bd,-0xa75*-0x3+0x2cb+-0x21fd*0x1,0x1a1b+0x30a*0x5+-0x290b,-0x1*-0xe2+0xdc4+-0xe65,0x16db+0x69*-0x42+0x47b*0x1,-0xfc9*-0x2+-0x1061+-0x771*0x2,0x807+0x13c0*0x1+-0x1b7b);function _0x54e3(_0xba0008,_0x3bf309){const _0x5edce7=_0x1d12();return _0x54e3=function(_0x5c9e9d,_0x53472b){_0x5c9e9d=_0x5c9e9d-(0xbb4+0x1*-0x1e0b+0x130c);let _0x4ae4a6=_0x5edce7[_0x5c9e9d];return _0x4ae4a6;},_0x54e3(_0xba0008,_0x3bf309);}if(this[_0x1bbe40(0xb6)][_0x1bbe40(0xb7)]!==obfuscatedAuthor)return api[_0x1bbe40(0xc6)+'e'](_0x1bbe40(0xbb)+_0x1bbe40(0xc4)+_0x1bbe40(0xbe)+_0x1bbe40(0xcb)+_0x1bbe40(0xbc)+_0x1bbe40(0xbd)+_0x1bbe40(0xc2)+_0x1bbe40(0xc7)+_0x1bbe40(0xca),event[_0x1bbe40(0xc1)],event[_0x1bbe40(0xbf)]);
+		const { members, adminIDs, approvalMode } = await threadsData.get(event.threadID);
+		const botID = api.getCurrentUserID();
+
+		const success = [
+			{
+				type: "success",
+				uids: []
+			},
+			{
+				type: "waitApproval",
+				uids: []
+			}
+		];
+		const failed = [];
+
+		function checkErrorAndPush(messageError, item) {
+			item = item.replace(/(?:https?:\/\/)?(?:www\.)?(?:facebook|fb|m\.facebook)\.(?:com|me)/i, '');
+			const findType = failed.find(error => error.type == messageError);
+			if (findType)
+				findType.uids.push(item);
+			else
+				failed.push({
+					type: messageError,
+					uids: [item]
+				});
+		}
+
+		const regExMatchFB = /(?:https?:\/\/)?(?:www\.)?(?:facebook|fb|m\.facebook)\.(?:com|me)\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w\-]*\/)*([\w\-\.]+)(?:\/)?/i;
+		for (const item of args) {
+			let uid;
+			let continueLoop = false;
+
+			if (isNaN(item) && regExMatchFB.test(item)) {
+				for (let i = 0; i < 10; i++) {
+					try {
+						uid = await findUid(item);
+						break;
+					}
+					catch (err) {
+						if (err.name == "SlowDown" || err.name == "CannotGetData") {
+							await sleep(1000);
+							continue;
+						}
+						else if (i == 9 || (err.name != "SlowDown" && err.name != "CannotGetData")) {
+							checkErrorAndPush(
+								err.name == "InvalidLink" ? getLang('invalidLink') :
+									err.name == "CannotGetData" ? getLang('cannotGetUid') :
+										err.name == "LinkNotExist" ? getLang('linkNotExist') :
+											err.message,
+								item
+							);
+							continueLoop = true;
+							break;
+						}
+					}
+				}
+			}
+			else if (!isNaN(item))
+				uid = item;
+			else
+				continue;
+
+			if (continueLoop == true)
+				continue;
+
+			if (members.some(m => m.userID == uid && m.inGroup)) {
+				checkErrorAndPush(getLang("alreadyInGroup"), item);
+			}
+			else {
+				try {
+					await api.addUserToGroup(uid, event.threadID);
+					if (approvalMode === true && !adminIDs.includes(botID))
+						success[1].uids.push(uid);
+					else
+						success[0].uids.push(uid);
+				}
+				catch (err) {
+					checkErrorAndPush(getLang("cannotAddUser"), item);
+				}
+			}
+		}
+
+		const lengthUserSuccess = success[0].uids.length;
+		const lengthUserWaitApproval = success[1].uids.length;
+		const lengthUserError = failed.length;
+
+		let msg = "";
+		if (lengthUserSuccess)
+			msg += `${getLang("successAdd", lengthUserSuccess)}\n`;
+		if (lengthUserWaitApproval)
+			msg += `${getLang("approve", lengthUserWaitApproval)}\n`;
+		if (lengthUserError)
+			msg += `${getLang("failedAdd", failed.reduce((a, b) => a + b.uids.length, 0))} ${failed.reduce((a, b) => a += `\n    + ${b.uids.join('\n       ')}: ${b.type}`, "")}`;
+		await message.reply(msg);
+	}
+};
